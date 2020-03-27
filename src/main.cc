fb0ecae9af4a0ed18c753961aa9b4f292c2957e1
Hovnatan Karapetyan <hovnatan@gmail.com>
2020-03-27 10:38:25 +0400

    Init

---
+++ src/main.cc
@@ -0,0 +1,19 @@
+#include <glib/gprintf.h>
+#include <i3ipc-glib/i3ipc-glib.h>
+
+int main() {
+  i3ipcConnection *conn;
+  gchar *reply;
+
+  conn = i3ipc_connection_new(NULL, NULL);
+
+  reply = i3ipc_connection_message(conn, I3IPC_MESSAGE_TYPE_COMMAND,
+                                   "focus left", NULL);
+
+  g_printf("Reply: %s\n", reply);
+
+  g_free(reply);
+  g_object_unref(conn);
+
+  return 0;
+}
